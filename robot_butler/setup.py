6a1be1d2fd865ced33a8ba607a875bdcc70e3716
kishore-saravanan <kishoresaravanan2002@gmail.com>
2025-01-30 17:42:08 +0530

    ROS2 Packages Creation

---
+++ robot_butler/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_butler'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wolf',
+    maintainer_email='kishoresaravanan2002@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
